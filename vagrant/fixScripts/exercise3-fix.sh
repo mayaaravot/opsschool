8bbb910b7655a30d8f02d338362910fd49bdfd62
mayaaravot <53709388+mayaaravot@users.noreply.github.com>
2019-08-13 12:49:33 +0300

    Update exercise3-fix.sh

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo sed -i 's/deny/Allow/g' /etc/apache2/sites-enabled/000-default
+sudo sed -i 's/deny/Allow/g' /etc/apache2/sites-enabled/000-default && sudo service apache2 reload && sudo service apache2 restart
